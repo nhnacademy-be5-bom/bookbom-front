6d1c03beae3c2213d6d91398569641f7d986173c
youaresogay22 <48512751+youaresogay22@users.noreply.github.com>
2024-03-15 11:19:15 +0900

    Update FrontApplication.java

--- src/main/java/shop/bookbom/front/FrontApplication.java
+++ src/main/java/shop/bookbom/front/FrontApplication.java
@@ -3,7 +3,7 @@ package shop.bookbom.front;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class FrontApplication {
 
 	public static void main(String[] args) {

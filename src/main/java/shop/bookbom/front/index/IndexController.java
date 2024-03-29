f684d7babd5ab58cdde9b4111d6eae460e910395
hyeond0 <a98792530@gmail.com>
2024-03-29 15:40:56 +0900

    [fix] Spring Security 의존성 제거

--- src/main/java/shop/bookbom/front/index/IndexController.java
+++ src/main/java/shop/bookbom/front/index/IndexController.java
@@ -1,20 +1,19 @@
 package shop.bookbom.front.index;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class IndexController {
-    @GetMapping(value = {"/index.html","/"})
-    public String index(HttpServletRequest request, Model model){
+    @GetMapping(value = {"/index.html", "/"})
+    public String index(HttpServletRequest request, Model model) {
         String ip = request.getHeader("x-forwarded-for");
-        if(ip == null ){
+        if (ip == null) {
             ip = request.getRemoteAddr();
         }
-        model.addAttribute("ip",ip);
+        model.addAttribute("ip", ip);
         return "index/index";
     }
 }

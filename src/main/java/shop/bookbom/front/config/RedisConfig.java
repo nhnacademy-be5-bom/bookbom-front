617efaab54deb9823eaf5948f56afaededeb5ac0
hyeond0 <a98792530@gmail.com>
2024-04-05 15:34:25 +0900

    [feat] Secure key Manager를 이용한 redis 접속 정보 암호화

--- src/main/java/shop/bookbom/front/config/RedisConfig.java
+++ src/main/java/shop/bookbom/front/config/RedisConfig.java
@@ -1,25 +1,42 @@
 package shop.bookbom.front.config;
 
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.client.RestTemplate;
+import shop.bookbom.front.config.dto.KeystoreResponse;
 
 @Configuration
 @EnableRedisRepositories
+@Import({SecureManagerConfig.class})
+@RequiredArgsConstructor
 public class RedisConfig {
-    @Value("${spring.redis.host}")
+
+    private final RestTemplate restTemplate;
+
+    @Value("${keymanager.url}")
+    private String keyManagerUrl;
+
+    @Value("${secure.manager.appkey}")
+    private String appkey;
+
+
+    @Value("${secure.manager.redis.host}")
     private String host;
 
-    @Value("${spring.redis.port}")
-    private int port;
+    @Value("${secure.manager.redis.port}")
+    private String port;
 
-    @Value("${spring.redis.password}")
+    @Value("${secure.manager.redis.password}")
     private String password;
 
     @Value("${spring.redis.database}")
@@ -27,8 +44,9 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
-        configuration.setPassword(password);
+        RedisStandaloneConfiguration configuration =
+                new RedisStandaloneConfiguration(getValue(host), Integer.parseInt(getValue(port)));
+        configuration.setPassword(getValue(password));
         configuration.setDatabase(Integer.parseInt(database));
         return new LettuceConnectionFactory(configuration);
     }
@@ -45,5 +63,11 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(stringRedisSerializer);
 
         return redisTemplate;
+    }
+
+    private String getValue(String key) {
+        String url = keyManagerUrl + appkey + "/secrets/" + key;
+        KeystoreResponse response = restTemplate.getForObject(url, KeystoreResponse.class);
+        return Objects.requireNonNull(response).getBody().getSecret();
     }
 }

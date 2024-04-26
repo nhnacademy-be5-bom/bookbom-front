7761e23deed37268a76aff3123de7a31e4bb4c57
hyeond0 <a98792530@gmail.com>
2024-04-26 14:22:55 +0900

    [fix] 장바구니 수량 업데이트 에러 수정
    - @RequestBody 매핑을 위해 기본 생성자 추가

--- src/main/java/shop/bookbom/front/domain/cart/dto/request/CartUpdateRequest.java
+++ src/main/java/shop/bookbom/front/domain/cart/dto/request/CartUpdateRequest.java
@@ -2,8 +2,10 @@ package shop.bookbom.front.domain.cart.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CartUpdateRequest {
     private int quantity;

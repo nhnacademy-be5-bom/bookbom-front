6cb6ad377011e9de93eac6cd7bcafb743f3ac9e7
hyeond0 <a98792530@gmail.com>
2024-04-30 16:29:04 +0900

    [feat] 주문 내역 페이징 기능 추가

--- src/main/java/shop/bookbom/front/domain/user/controller/UserController.java
+++ src/main/java/shop/bookbom/front/domain/user/controller/UserController.java
@@ -2,11 +2,14 @@ package shop.bookbom.front.domain.user.controller;
 
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import shop.bookbom.front.domain.order.dto.response.OrderInfoResponse;
 import shop.bookbom.front.domain.user.service.UserService;
 
 @Controller
@@ -17,13 +20,16 @@ public class UserController {
     @GetMapping("/users/order")
     public String orderList(
             Model model,
-            Pageable pageable,
+            @PageableDefault Pageable pageable,
             @RequestParam(value = "date_from", required = false) String dateFrom,
             @RequestParam(value = "date_to", required = false) String dateTo
     ) {
         LocalDate orderDateMin = dateFrom == null ? null : LocalDate.parse(dateFrom);
         LocalDate orderDateMax = dateTo == null ? null : LocalDate.parse(dateTo);
-        model.addAttribute("orders", userService.getOrderList(orderDateMin, orderDateMax, pageable));
+        Page<OrderInfoResponse> orderPage = userService.getOrderList(orderDateMin, orderDateMax, pageable);
+        model.addAttribute("orderPage", orderPage);
+        model.addAttribute("dateFrom", dateFrom);
+        model.addAttribute("dateTo", dateTo);
         return "page/user/order-list";
     }
 }

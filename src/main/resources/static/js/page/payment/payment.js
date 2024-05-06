ef8ac5f41915a99be25081e34b15c55d3ca2e8dc
boyun <bu339@naver.com>
2024-05-06 22:04:31 +0900

    [fix] 토스페이 성공, 실패 url 수정

--- src/main/resources/static/js/page/payment/payment.js
+++ src/main/resources/static/js/page/payment/payment.js
@@ -33,9 +33,6 @@ var amount = document.getElementById("amount").value;
 function handlePayment() {
     const isTossPaySelected = tossPayButton.classList.contains('active');
     if (isTossPaySelected) {
-        // 토스 페이로 결제 처리 로직 추가
-        // window.location.href = '/toss-pay';
-        // window.open('/toss-pay', '_blank');
         tossPayments
             .requestPayment('카드', {
                 // 결제수단
@@ -43,8 +40,8 @@ function handlePayment() {
                 amount: amount,
                 orderId: orderId,
                 orderName: orderName,
-                successUrl: 'http://localhost:8020/toss-success',
-                failUrl: 'http://localhost:8020/toss-fail',
+                successUrl: 'https://bookbom.shop/toss-success',
+                failUrl: 'https://bookbom.shop/toss-fail',
                 flowMode: 'DIRECT',
                 easyPay: '토스페이',
 
@@ -64,53 +61,3 @@ function handlePayment() {
 }
 
 
-//     const paymentSelectButtons = document.querySelectorAll('.payment-select');
-//     const paymentButton = document.getElementById('payment-button');
-//     const tossPayButton = document.getElementById('toss-pay');
-//
-//     paymentSelectButtons.forEach(button => {
-//         button.addEventListener('click', () => {
-//             // 현재 클릭된 버튼에 active 클래스를 추가
-//             paymentSelectButtons.forEach(btn => {
-//                 btn.classList.remove('active');
-//                 btn.style.border = '1px solid #ffffff';
-//             });
-//             button.classList.add('active');
-//             button.style.border = '2px solid #333d4b';
-//
-//             // 결제하기 버튼을 활성화
-//             paymentButton.disabled = false;
-//         });
-//     });
-//
-//     function handlePayment() {
-//         const isTossPaySelected = tossPayButton.classList.contains('active');
-//         if (isTossPaySelected) {
-//             // 토스 페이로 결제 처리 로직 추가
-//             // window.location.href = '/toss-pay';
-//             window.open('/toss-pay', '_blank');
-//
-// //         const currentURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname.split("/")[1];
-// // // const generateRandomString = () => window.btoa(Math.random()).slice(0, 20);
-// //         var amount = 50000;
-// //
-// //         const clientKey = "test_ck_ex6BJGQOVDbEXGJvJJ558W4w2zNb";
-// // // const customerKey = generateRandomString();
-// // // const paymentWidget = PaymentWidget(clientKey, customerKey); // 회원 결제
-// //         const paymentWidget = PaymentWidget(clientKey, PaymentWidget.ANONYMOUS); // 비회원 결제
-// //         console.log('토스 페이로 결제합니다.');
-// //         paymentWidget.requestPayment({
-// //             orderId: generateRandomString(),
-// //             orderName: "토스 티셔츠 외 2건",
-// //             successUrl: "http://localhost:8020/toss/success",
-// //             failUrl: "http://localhost:8020/toss/fail",
-// //             customerEmail: "customer123@gmail.com",
-// //             customerName: "김토스",
-// //             customerMobilePhone: "01012341234",
-// //         });
-//
-//         } else {
-//             console.log('토스 페이로 선택되지 않았습니다.');
-//             // 토스 페이가 선택되지 않은 경우에 대한 처리 추가
-//         }
-//     }

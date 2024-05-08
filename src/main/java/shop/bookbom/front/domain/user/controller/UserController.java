c7f92b1f8577bd3d3f031391f82de40133801d17
hyeond0 <a98792530@gmail.com>
2024-05-09 00:10:39 +0900

    [feat] 회원가입 폼 구현

--- src/main/java/shop/bookbom/front/domain/user/controller/UserController.java
+++ src/main/java/shop/bookbom/front/domain/user/controller/UserController.java
@@ -1,15 +1,20 @@
 package shop.bookbom.front.domain.user.controller;
 
 import java.time.LocalDate;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import shop.bookbom.front.domain.order.dto.response.OrderInfoResponse;
+import shop.bookbom.front.domain.user.dto.request.SignUpRequest;
 import shop.bookbom.front.domain.user.service.UserService;
 
 @Controller
@@ -31,5 +36,22 @@ public class UserController {
         model.addAttribute("dateFrom", dateFrom);
         model.addAttribute("dateTo", dateTo);
         return "page/user/order-list";
+    }
+
+    @GetMapping("/sign-up")
+    public String signUpPage(@ModelAttribute("signUpRequest") SignUpRequest member) {
+        return "page/user/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUp(@ModelAttribute @Valid SignUpRequest signUpRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "page/user/sign-up";
+        }
+        if (signUpRequest.getBirthDate() != null && signUpRequest.getBirthDate().isAfter(LocalDate.now())) {
+            bindingResult.rejectValue("birthDate", "error.birthDate", "생년월일은 현재 날짜보다 이후일 수 없습니다.");
+            return "page/user/sign-up";
+        }
+        return "redirect:/";
     }
 }
---
+++ src/main/java/shop/bookbom/front/domain/user/dto/request/SignUpRequest.java
@@ -0,0 +1,48 @@
+package shop.bookbom.front.domain.user.dto.request;
+
+import java.time.LocalDate;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SignUpRequest {
+    @NotBlank(message = "아이디를 입력하세요.")
+    @Email(message = "유효한 이메일 주소를 입력하세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    private String password;
+
+    @Size(min = 2, message = "이름은 최소 2자 이상이어야 합니다.")
+    private String name;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "생년월일을 입력하세요.")
+    private LocalDate birthDate;
+
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "000-0000-0000 양식으로 입력해주세요.")
+    private String phoneNumber;
+
+    @NotEmpty(message = "닉네임을 입력해주세요.")
+    private String nickname;
+
+    @Size(min = 5, message = "우편번호는 최소 5자 이상이어야 합니다")
+    private String addressNumber;
+
+    @NotEmpty(message = "주소를 입력해주세요.")
+    private String address;
+
+    @NotEmpty(message = "상세주소를 입력해주세요.")
+    private String addressDetail;
+}

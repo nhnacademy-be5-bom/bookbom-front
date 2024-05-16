02b70a55eaddcea369b123386a6df534040b3385
dudxor4587 <dudxor4587@gmail.com>
2024-05-16 16:11:52 +0900

    [feature] 정책 변경 시 해당 정책 반영

--- src/main/java/shop/bookbom/front/domain/user/dto/response/UserRankResponse.java
+++ src/main/java/shop/bookbom/front/domain/user/dto/response/UserRankResponse.java
@@ -3,12 +3,12 @@ package shop.bookbom.front.domain.user.dto.response;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import shop.bookbom.front.domain.rank.entity.Rank;
+import shop.bookbom.front.domain.rank.entity.dto.request.RankRequest;
 
 @Getter
 @NoArgsConstructor
 public class UserRankResponse {
     private String nickname;
     private String userrank;
-    private List<Rank> ranks;
+    private List<RankRequest> ranks;
 }

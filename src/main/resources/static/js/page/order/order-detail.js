b765456fdc8405d6766791f0798acef2fd5e34ff
hyeond0 <a98792530@gmail.com>
2024-05-16 21:03:29 +0900

    [feat] 관리자 계정도 장바구니 가능하도록 수정

--- src/main/resources/static/js/page/order/order-detail.js
+++ src/main/resources/static/js/page/order/order-detail.js
@@ -5,44 +5,45 @@ function openReviewModal(bookId) {
     myModal.show();
 }
 
-document.querySelectorAll('.review').forEach(item => {
-    item.addEventListener('click', event => {
-        const bookId = item.getAttribute('data-book-id');
-        let orderId = window.location.pathname.split('/').pop();
-        const url = `/reviews/exists-check?bookId=${bookId}&orderId=${orderId}`;
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.review').forEach(item => {
+        item.addEventListener('click', event => {
+            const bookId = item.getAttribute('data-book-id');
+            let orderId = window.location.pathname.split('/').pop();
+            const url = `/reviews/exists-check?bookId=${bookId}&orderId=${orderId}`;
+            fetch(url)
+                .then(response => {
+                    if (!response.ok) {
+                        alert('리뷰를 작성할 수 없습니다. 관리자에게 문의 주세요.');
+                        return;
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data)
+                    if (data.header.isSuccessful === false) {
+                        alert('리뷰를 작성할 수 없습니다. 관리자에게 문의 주세요.');
+                        return;
+                    }
+                    if (data.result.exists === true) {
+                        alert('이미 리뷰를 작성하셨습니다.');
+                        return;
+                    }
+                    openReviewModal(bookId);
+                })
+                .catch(error => {
                     alert('리뷰를 작성할 수 없습니다. 관리자에게 문의 주세요.');
-                    return;
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log(data)
-                if (data.header.isSuccessful === false) {
-                    alert('리뷰를 작성할 수 없습니다. 관리자에게 문의 주세요.');
-                    return;
-                }
-                if (data.result.exists === true) {
-                    alert('이미 리뷰를 작성하셨습니다.');
-                    return;
-                }
-                openReviewModal(bookId);
-            })
-            .catch(error => {
-                alert('리뷰를 작성할 수 없습니다. 관리자에게 문의 주세요.');
-                console.error('Error:', error);
-            });
+                    console.error('Error:', error);
+                });
+        });
     });
-});
 
-document.querySelector('.write-review').addEventListener('click', function () {
-    let bookId = document.getElementById('bookIdField').value;
-    let orderId = window.location.pathname.split('/').pop();
-    let reviewType = document.getElementById('reviewType').value;
-    window.location.href = `/reviews?bookId=${bookId}&orderId=${orderId}&reviewType=${reviewType}`;
-});
+    document.querySelector('.write-review').addEventListener('click', function () {
+        let bookId = document.getElementById('bookIdField').value;
+        let orderId = window.location.pathname.split('/').pop();
+        let reviewType = document.getElementById('reviewType').value;
+        window.location.href = `/reviews?bookId=${bookId}&orderId=${orderId}&reviewType=${reviewType}`;
+    });
 // document.addEventListener("DOMContentLoaded", function () {
 //     document.querySelector('.confirm-delete').addEventListener('click', function () {
 //         const orderId = window.location.pathname.split('/').pop();
@@ -63,7 +64,8 @@ document.querySelector('.write-review').addEventListener('click', function () {
 //         });
 //     });
 // });
-document.querySelector('.confirm-delete').addEventListener('click', function () {
-    // 팝업 창으로 페이지 열기
-    window.open('/order/cancel', '_blank', 'width=600,height=400');
+    document.querySelector('.confirm-delete').addEventListener('click', function () {
+        // 팝업 창으로 페이지 열기
+        window.open('/order/cancel', '_blank', 'width=600,height=400');
+    });
 });

55392139e12a22111b6fe356480da5b1e5a5b1c9
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2024-05-16 17:31:07 +0900

    [feat] 비회원, 관리자 옵션 추가

--- src/main/java/shop/bookbom/front/domain/signin/service/SigninService.java
+++ src/main/java/shop/bookbom/front/domain/signin/service/SigninService.java
@@ -1,8 +1,10 @@
 package shop.bookbom.front.domain.signin.service;
 
 
+import java.util.Collection;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,26 @@ public class SigninService {
             return false;
         }
         return authentication.isAuthenticated();
+    }
+
+    public boolean isUser() {
+        return hasRole("USER");
+    }
+
+    public boolean isAdmin() {
+        return hasRole("ADMIN");
+    }
+
+    public boolean hasRole(String role) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            return false;
+        }
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        boolean hasRoleMember = authorities.stream()
+                .anyMatch(authority -> authority.getAuthority().equals("ROLE_" + role));
+
+        return hasRoleMember;
     }
 
 }

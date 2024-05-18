ff98e2aed6e25ce1d545c5b48042059976b9f821
hyeond0 <a98792530@gmail.com>
2024-05-18 16:04:50 +0900

    [feat] 메인 페이지, 카테고리 헤더 캐시 적용

--- src/main/java/shop/bookbom/front/index/service/impl/IndexServiceImpl.java
+++ src/main/java/shop/bookbom/front/index/service/impl/IndexServiceImpl.java
@@ -1,6 +1,8 @@
 package shop.bookbom.front.index.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -10,15 +12,18 @@ import shop.bookbom.front.index.service.IndexService;
 
 @Service
 @RequiredArgsConstructor
+@CacheConfig(cacheNames = "main")
 public class IndexServiceImpl implements IndexService {
     private final IndexAdapter indexAdapter;
 
     @Override
+    @Cacheable(key = "'latestBooks'", condition = "#pageable.pageNumber == 0 && #pageable.pageSize == 8")
     public Page<BookSearchResponse> mainLatestBooks(Pageable pageable) {
         return indexAdapter.mainLatestBooks(pageable);
     }
 
     @Override
+    @Cacheable(key = "'bestBooks'", condition = "#pageable.pageNumber == 0 && #pageable.pageSize == 8")
     public Page<BookSearchResponse> mainBestBooks(Pageable pageable) {
         return indexAdapter.mainBestBooks(pageable);
     }

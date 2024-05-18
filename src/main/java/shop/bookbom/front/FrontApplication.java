75fb0472677f37b5795f7bc24fbbefcd33e84a9f
hyeond0 <a98792530@gmail.com>
2024-05-18 16:00:42 +0900

    [feat] Spring Cache 설정 적용

--- src/main/java/shop/bookbom/front/FrontApplication.java
+++ src/main/java/shop/bookbom/front/FrontApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableFeignClients
 @ConfigurationPropertiesScan
+@EnableCaching
 public class FrontApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/shop/bookbom/front/config/CacheConfig.java
@@ -0,0 +1,18 @@
+package shop.bookbom.front.config;
+
+import java.util.List;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
+        cacheManager.setAllowNullValues(false);
+        cacheManager.setCacheNames(List.of("category", "main"));
+        return cacheManager;
+    }
+}
